d6e4ce3612b89a3fb646cfa9652c005c55eab9d5
likanug <likanug.g@qq.com>
2025-08-01 09:17:24 +0800

    添加一些数组和切片示例代码

--- src/main.zig
+++ src/main.zig
@@ -3,24 +3,18 @@ const hello_world = @import("hello_world");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try hello_world.bufferedPrint();
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    //std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    //try hello_world.bufferedPrint();
+    const a = [_]u8{ 1, 2, 3 };
+    const b = [_]u8{ 4, 5 };
+    const d = b ++ a;
+    std.debug.print("{any}\n", .{a ++ b}); // { 1, 2, 3, 4, 5 }
+    std.debug.print("{any}\n", .{d}); // { 4, 5, 1, 2, 3 }
+    const ns = [4]u8{ 48, 24, 12, 6 };
+    const sl = ns[1..3];
+    std.debug.print("{any}\n", .{sl}); // { 24, 12 }
+    const s2 = ns[1..];
+    std.debug.print("{any}\n", .{s2}); // {24, 12, 6}
+    const s3 = ns ** 2;
+    std.debug.print("{any}\n", .{s3}); // { 48, 24, 12, 6, 48, 24, 12, 6 }
 }
