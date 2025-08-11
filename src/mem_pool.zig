507f180c98253b9fcd4f7e4b6a653a2bb9e66c5d
likanug <likanug.g@qq.com>
2025-08-11 22:48:30 +0800

    增加使用悬空指针的示例

--- src/mem_pool.zig
+++ src/mem_pool.zig
@@ -19,3 +19,37 @@ test "use memory pool" {
     _ = p1;
     _ = p3;
 }
+
+test "MemoryPool uninitialized vs initialized" {
+    var pool = std.heap.MemoryPool(u32).init(std.heap.page_allocator);
+    defer pool.deinit();
+
+    // 分配 p1 和 p2
+    const p1 = try pool.create();
+    const p2 = try pool.create();
+
+    // 给 p2 一个确定的值
+    p2.* = 42;
+
+    // 分配 p3（为了和 p2 中间隔一点）
+    const p3 = try pool.create();
+
+    // 释放 p2
+    pool.destroy(p2);
+
+    // 再申请一块（会复用 p2 的地址）
+    const p4 = try pool.create();
+    std.debug.print("\np2 地址: {p}\n", .{p2}); // u32@1ed44e70018 当然每台机器上运行的结果都不同
+    std.debug.print("\np2 当前值(未初始化): {}\n", .{p2.*}); // 2863311530 当然每台机器上运行的结果都不同
+    std.debug.print("\np4 地址: {p}\n", .{p4}); // u32@1ed44e70018 当然每台机器上运行的结果都不同
+    std.debug.print("p4 当前值(未初始化): {}\n", .{p4.*}); //2863311530  一个随机值，同样每台机器上也不相同
+
+    // 初始化 p4
+    p4.* = 99;
+    try std.testing.expect(p4.* == 99);
+    try std.testing.expect(p2.* == 99);
+    try std.testing.expect(p2.* != 42);
+
+    _ = p1;
+    _ = p3;
+}
