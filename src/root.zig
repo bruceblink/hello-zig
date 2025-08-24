55e45bfe424b4901e9afea0e985b811932cf8e26
likanug <likanug.g@qq.com>
2025-08-24 11:59:04 +0800

    添加comptime示例

--- src/root.zig
+++ src/root.zig
@@ -181,3 +181,17 @@ test "slice 2" {
     const slice = array[0..3];
     try expect(*const [3]u8 == @TypeOf(slice));
 }
+
+fn fibonacci(n: u16) u16 {
+    if (n == 0 or n == 1) return n;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+test "comptime blocks" {
+    const x = comptime fibonacci(10);
+    const y = comptime blk: {
+        break :blk fibonacci(10);
+    };
+    try expect(y == 55);
+    try expect(x == y);
+}
